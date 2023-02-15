22dcb9b47ca35b505bc5e437f2543114d48cc2c0
zahraka <zahrakarvan@gmail.com>
2023-02-15 19:04:56 +0330

    edit codes

--- Q-1/app.js
+++ Q-1/app.js
@@ -16,3 +16,10 @@ console.log(firstTd2);
 console.log(form);
 console.log(firstInput);
 console.log(lastInput);
+
+// 1. The table with id="age-table".
+// 2. All label elements inside that table (there should be 3 of them).
+// 3. The first td in that table (with the word “Age”).
+// 4. The form with name="search".
+// 5. The first input in that form.
+// 6. The last input in that form.
