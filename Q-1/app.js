6b3c5b95f190aa8ee2aa8642b5724eb1d71eb61a
zahraka <zahrakarvan@gmail.com>
2023-02-15 18:59:10 +0330

    edit js file

--- Q-1/app.js
+++ Q-1/app.js
@@ -13,21 +13,6 @@ console.log(table);
 console.log(label);
 console.log(firstTd);
 console.log(firstTd2);
-
 console.log(form);
 console.log(firstInput);
 console.log(lastInput);
-
-///---------1-2---------
-const externalLink = document.getElementById("external-link");
-externalLink.style.color = orange;
-
-function colorLinks(hex) {
-  var links = document.getElementsByTagName("external-link");
-  for (var i = 0; i < links.length; i++) {
-    if (links[i].href) {
-      links[i].style.color = hex;
-    }
-  }
-}
-colorLinks(orange);
